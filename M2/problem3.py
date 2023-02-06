a86ce700a25d671778ab6154e3307909050ddc1d
karthik9 <sk3374@njit.edu>
2023-02-06 18:51:49 -0500

    added the working solution

--- M2/problem3.py
+++ M2/problem3.py
@@ -3,25 +3,18 @@ a2 = [-1, 1, -2, 2, 3, -3, -4, 5]
 a3 = [-0.01, -0.0001, -.15]
 a4 = ["-1", "2", "-3", "4", "-5", "5", "-6", "6", "-7", "7"]
 
-
 def process_array(num, arr):
     print("\nProcessing Array({}): \n\n".format(num))
     print(arr)
     print("\nPositive Output:\n")
-    for x in arr:
-        if type(x) == int:
-            if x > 0:
-                print(x, end=" ")
-        elif type(x) == float:
-            if x > 0.0:
-                print("{:.4f}".format(x), end=" ")
-        elif type(x) == str:
-            if x.startswith("-"):
-                continue
-            else:
-                print(x, end=" ")
-        print("\n")
     # TODO add new code here to print the desired result
+    final_arr = []
+    for value in arr:
+        if type(value) == int or type(value) == float:
+            final_arr.append(abs(value))
+        elif type(value) == str:
+            final_arr.append(str(abs(int(value))))
+    print(final_arr)
 
 
 print("Problem 3")
