8a4dc2df0525cc7c08c18585cb5975fbe0b866d3
karthik <sk3374@njit.edu>
2023-02-25 20:58:36 -0500

    working commit 1

--- M4/MyCalc.py
+++ M4/MyCalc.py
@@ -4,57 +4,92 @@ class MyCalc:
     def add(a,b): 
         ans = a+b
         return ans
-       
-    def minus(a,b): 
+    #UCID: sk3374
+    #Date: 2/23/23 9:00 PM
+    #function add() takes in 2 parameters a and b, adds them and stores the result in ans
+    #In the end returns ans
+
+    def sub(a,b): 
         ans = a-b
         return ans
-    
+    #UCID: sk3374
+    #Date: 2/23/23 9:02 PM
+    #function sub() takes in 2 parameters a and b, does a minus b and stores the result in ans
+    #In the end returns ans
+
     def mul(a,b): 
         ans = a*b
         return ans
+    #UCID: sk3374
+    #Date: 2/23/23 9:05 PM
+    #function mul() takes in 2 parameters a and b, does a times b and stores the result in ans
+    #In the end returns ans
     
     def div(a,b): 
         if b == 0:
-            raise ValueError("Cannot divide by zero. Please change the denominator")
+            print("Cannot divide by zero. Please change the denominator")
+            return
         ans = a/b
         return ans
-    
+    #UCID: sk3374
+    #Date: 2/23/23 9:07 PM
+    #function div() takes in 2 parameters a and b, does a divided by b and stores the result in ans
+    #In the end returns ans
+        
     do_math = {
         "+": add,
-        "-": minus,
+        "-": sub,
         "*": mul,
         "/": div 
     }
+    #UCID: sk3374
+    #Date: 2/23/23 8:43 PM
+    #Used a dictionary and stored the operators as key and the reference to their functions as values
+
+    def calculate(self, num1, num2, op):
+        if num1  == "ans":
+            return self.calculate(self.ans, num2, op)
+        else:
+            try:
+                num1 = float(num1)
+                num2 = float(num2) 
+            except:
+                print("Please use valid numbers in the equation")
+                return
+            self.ans = MyCalc.do_math[op](num1, num2)
+        return self.ans        
+    #UCID: sk3374
+    #Date: 2/25/23 8:23 PM
+    #function calculate() takes in 4 arguments (including self) 
+    #checks if the num1 is equal to ans, if yes, takes the previous result ans as num1 
+    #else, we convert both the nums to float and if it fails we send a prompt to use and return
+    #if the conversion is successfull, we call the appropriate function using do_math dict   
+    
 
 def main():
-    print("Simple Calculator: Supports basic math operations between 2 numbers")
+    print("Simple Calculator: Supports basic math operations between 2 numbers (like 4/5)")
     
-    Calc = MyCalc()
+    calc = MyCalc()
 
     while True:
-        decide = input("Press 'Y' to do Math and 'Q' to quit: ") 
-        if decide in ('Y', 'y'):
-            one = (input("Enter first num (or ""ans"" to use the previous result): "))
-            
-            try:
-                one = float(Calc.ans) if one.strip() == "ans" else float(one)
-                two = float(input("Enter second num: "))
-            except ValueError:
-                print("Only Integers are allowed in the Calculator")
-                return
-            operator = input("operation to be performed (+,-,*,/): ")
-
-            if operator not in Calc.do_math.keys():
-                print("Invalid operation. Expected values : +,-,*,/")
-                return
-
-            Calc.ans = float(Calc.do_math[operator](one,two))
-            print(f"Result = {Calc.ans}")      
-        elif decide in ('Q', 'q'):
-            print("Bye!")
+        decide = input("Press 'M' to do math or 'Q' to quit: ") 
+        if decide in ('M', 'm'):
+            eq  = input("Enter the equation: ")
+            for op in calc.do_math.keys():
+                if op in eq:
+                    nums = eq.split(op)
+                    r = calc.calculate(nums[0].strip(), nums[1].strip(), op)
+                    print("Result: " + str(r))
+        if decide in ('Q','q'):
+            print("GoodBye")
             return
-        else:
-            print('Invalid Keyword. Expected values: Y,y,Q,q')
-    
+        
 if __name__ == "__main__":
     main()
+    #UCID: sk3374
+    #Date: 2/25/23 8:13 PM
+    #the main function asks user to press 'M' to do the math or 'Q' to quit 
+    #if user presses 'M' we check for the operation involved in the equation using a for op in keys()  
+    #when there is a match we split the equation that is stored in the string and store them in nums
+    #we then send these values after removing whitespaces to the calculate() function using an object calc of MyCalc class.   
+ 
--- M4/test_MyCalc.py
+++ M4/test_MyCalc.py
@@ -1,11 +1,75 @@
 from MyCalc import MyCalc
 
-calc = MyCalc
+calc = MyCalc()
 
-def test_add():
-    check = calc.add(3,4)
+def test_number_add_number():
+    check = calc.calculate(3,4,'+')
     assert check == 7
-
-def test_add_ans():
-    check = calc.add("ans",3)
+    #UCID: sk3374
+    #Date: 2/25/23 8:30 PM
+    #function test_number_add_number() calls calculate() function using an object calc of class MyClass
+    #we use test parameters 3,4,+ for testing addition.
+    #then we use assert to compare the result the function returns and the expected result (i.e 7 in our case)
+    
+def test_ans_add_number(): 
+    check = calc.calculate("ans",3,'+')
     assert check == 10
+    #UCID: sk3374
+    #Date: 2/25/23 8:39 PM
+    #function test_ans_add_number() calls calculate() function using an object calc of class MyClass
+    #we use test parameters "ans",3,- for testing addition when we "ans" as num1.
+    #then we use assert to compare the result the function returns and the expected result (i.e 10 in our case)    
+
+def test_number_sub_number():
+    check = calc.calculate(2,9,'-')
+    assert check == -7
+    #UCID: sk3374
+    #Date: 2/25/23 8:46 PM
+    #function test_number_sub_number() calls calculate() function using an object calc of class MyClass
+    #we use test parameters 2,9,- for testing subtraction.
+    #then we use assert to compare the result the function returns and the expected result (i.e -7 in our case)
+
+def test_ans_sub_number():
+    check = calc.calculate("ans",-7,'-')
+    assert check == 0
+    #UCID: sk3374
+    #Date: 2/25/23 8:55 PM
+    #function test_ans_sub_number() calls calculate() function using an object calc of class MyClass
+    #we use test parameters "ans",-7,- for testing subtraction when we "ans" as num1.
+    #then we use assert to compare the result the function returns and the expected result (i.e 0 in our case)
+
+def test_number_mult_number():
+    check = calc.calculate(3,2,'*')
+    assert check == 6
+    #UCID: sk3374
+    #Date: 2/25/23 9:13 PM
+    #function test_number_mult_number() calls calculate() function using an object calc of class MyClass
+    #we use test parameters 3,2,* for testing multiplication.
+    #then we use assert to compare the result the function returns and the expected result (i.e 6 in our case)
+
+def test_ans_mult_number():
+    check = calc.calculate("ans",3,'*')
+    assert check == 18
+    #UCID: sk3374
+    #Date: 2/25/23 9:23 PM
+    #function test_ans_mult_number() calls calculate() function using an object calc of class MyClass
+    #we use test parameters "ans",3,'*' for testing multiplication when "ans" is used as num1.
+    #then we use assert to compare the result the function returns and the expected result (i.e 18 in our case)
+
+def test_number_div_number():
+    check = calc.calculate(18,9,'/')
+    assert check == 2
+    #UCID: sk3374
+    #Date: 2/25/23 9:30 PM
+    #function test_number_div_number() calls calculate() function using an object calc of class MyClass
+    #we use test parameters 18,9,/ for testing division.
+    #then we use assert to compare the result the function returns and the expected result (i.e 2 in our case)
+
+def test_ans_div_number():
+    check = calc.calculate("ans",4,'/')
+    assert check == 0.5
+    #UCID: sk3374
+    #Date: 2/25/23 9:30 PM
+    #function test_ans_div_number() calls calculate() function using an object calc of class MyClass
+    #we use test parameters "ans",4,/ for testing division.
+    #then we use assert to compare the result the function returns and the expected result (i.e 0.5 in our case)
