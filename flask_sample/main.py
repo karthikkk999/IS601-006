ebda30f63deaa30c590eb9c7bb76874a302d9927
karthik <sk3374@njit.edu>
2023-03-06 19:37:23 -0500

    added comment to the test case

--- flask_sample/main.py
+++ flask_sample/main.py
@@ -4,7 +4,6 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     name = request.args.get('name', 'World')
--- flask_sample/test/test_dummy.py
+++ flask_sample/test/test_dummy.py
@@ -1,2 +1,3 @@
+#just a dummy test case for the build to pass
 def test_dummy():
     pass
