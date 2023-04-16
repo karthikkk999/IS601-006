df3fb15f43a8051e86ec03216053c12993fc4ba9
karthikkk999 <sk3374@njit.edu>
2023-04-15 22:30:51 -0400

    added the valid pssd check

--- Project/auth/forms.py
+++ Project/auth/forms.py
@@ -1,13 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, SubmitField, StringField
-from wtforms.validators import DataRequired, Email, InputRequired, EqualTo, Length, Optional
+from wtforms.validators import DataRequired, Email, InputRequired, EqualTo, Length, Optional, Regexp
 
 class RegisterForm(FlaskForm):
     username = StringField("username", validators=[DataRequired(), Length(2, 30)])
     email = EmailField("email", validators=[DataRequired(), Email()])
-    password = PasswordField("password", validators=[InputRequired(), EqualTo('confirm', message='Passwords must match')])
+    # password = PasswordField("password", validators=[InputRequired(), EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField("confirm", validators=[DataRequired()])
     submit = SubmitField("Register")
+    password = PasswordField("password", 
+                             validators=[InputRequired(), Length(min=8), 
+                             Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$', 
+                             message='Password must contain at least one uppercase letter, one lowercase letter and one digit')])
+
 
 class LoginForm(FlaskForm):
     email = StringField("email or username", validators=[DataRequired()]) #EmailField("email", validators=[DataRequired(), Email()])
